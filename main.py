27dd28ef856509094de74df64fa5cceb8b872825
Alexander_Acosta <alexanderacosta@unach.cl>
2021-08-27 17:55:23 -0400

    Avances de main

--- main.py
+++ main.py
@@ -2,7 +2,29 @@
 
 clients = 'pablo,ricardo,'
 
+def create_client(client_name):
+    global clients
+
+    clients += client_name
+    _add_coma()
+
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+def _add_coma():
+    global clients
+
+    clients += ','
+
 
 if __name__ == '__main__':
-    clients += 'david'
+    list_clients()
+
+    create_client('David')
+
+    list_clients()
+
     print(clients)
