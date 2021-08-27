033a2fa2dad06873a291d1e3d51ab7ebf5225fac
Alexander_Acosta <alexanderacosta@unach.cl>
2021-08-27 18:04:17 -0400

    Avance del programa

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ clients = 'pablo,ricardo,'
 def create_client(client_name):
     global clients
 
-    clients += client_name
-    _add_coma()
+    if client_name not in clients:
+        clients += client_name
+        _add_coma()
+    else:
+        print('Client already in the client\'s list')
 
 
 def list_clients():
@@ -19,12 +22,24 @@ def _add_coma():
 
     clients += ','
 
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+
 
 if __name__ == '__main__':
-    list_clients()
+    _print_welcome()
 
-    create_client('David')
+    command = input()
 
-    list_clients()
-
-    print(clients)
+    if command == 'C':
+        client_name = input('What is the client name?')
+        create_client(client_name)
+        list_clients()
+    elif command =='D':
+        pass
+    else:
+        print('Invalid command')
