865e5a916fc9b205e11d62a54bc6750055640bc0
Alexander_Acosta <alexanderacosta@unach.cl>
2021-08-29 00:05:00 -0400

    mas archivos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
 
-
-clients = 'pablo,ricardo,alexander'
+clients = 'pablo,ricardo,alexander,'
 clients = clients.capitalize()
 
 
@@ -65,7 +65,19 @@ def _print_welcome():
 
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name? ')
+
+        if client_name == 'exit':
+            client_name = None
+            break
+
+    if not client_name:
+        sys.exit()
+
+    return  client_name
 
 if __name__ == '__main__':
     _print_welcome()
